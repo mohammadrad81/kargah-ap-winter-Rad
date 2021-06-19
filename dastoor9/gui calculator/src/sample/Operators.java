80288fc7abe70fe7cd52e11c850bb1fb65eccddb
mohammad <70915350+mohammadrad81@users.noreply.github.com>
2021-06-19 17:15:27 +0430

    dastoor kar 9 kargah ( graphical calculator )

---
+++ dastoor9/gui calculator/src/sample/Operators.java
@@ -0,0 +1,18 @@
+package sample;
+
+/**
+ * an enum for operators of the calculator 
+ * @author Mohammad Heydari Rad
+ * @since 6/19/2021
+ * @version 1.0
+ */
+public enum Operators {
+    addition,
+    subtraction,
+    multiplication,
+    divination,
+    sin,
+    cos,
+    tan,
+    pow,
+}
